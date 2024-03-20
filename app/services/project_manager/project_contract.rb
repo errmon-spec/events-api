7b32c5d93bc032d1a342869ed8a44e2ecc54ed5e
Lucas Damasceno <lucas.damasceno@al.infnet.edu.br>
2024-03-20 04:10:12 +0000

    refactor ProjectContract - use Types::StrippedString

--- app/services/project_manager/project_contract.rb
+++ app/services/project_manager/project_contract.rb
@@ -3,8 +3,8 @@
 module ProjectManager
   class ProjectContract < ApplicationContract
     json do
-      required(:project_id).filled(:string)
-      required(:token).filled(:string)
+      required(:project_id).filled(Types::StrippedString)
+      required(:token).filled(Types::StrippedString)
     end
   end
 end
