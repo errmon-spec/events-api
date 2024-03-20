31d757cb8d6e2ea01b9ba08c55ec96517835b10e
Lucas Damasceno <lucas.damasceno@al.infnet.edu.br>
2024-03-20 02:20:18 +0000

    refactor ProjectManagerTest

--- test/services/project_manager_test.rb
+++ test/services/project_manager_test.rb
@@ -3,13 +3,16 @@
 require 'test_helper'
 
 class ProjectManagerTest < ActiveSupport::TestCase
-  test 'calls ProjectManager::ProjectSync.call' do
+  test 'calls ProjectSync#call' do
     payload = { project_id: SecureRandom.uuid, token: SecureRandom.hex(20) }
+
     mock = Minitest::Mock.new
     mock.expect :call, nil, [payload]
+
     ProjectManager::ProjectSync.stub :call, mock do
       ProjectManager.sync(payload)
     end
-    mock.verify
+
+    assert_mock mock
   end
 end
