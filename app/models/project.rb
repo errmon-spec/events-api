45fdbf940d44540ddf9abc2ea7b53fe91637da28
Lucas Damasceno <lucas.damasceno@al.infnet.edu.br>
2024-03-23 14:20:44 -0300

    test project_id uniqueness

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@
 
 class Project < ApplicationRecord
   validates :project_id, :token, presence: true
+  validates :project_id, uniqueness: true
 
   scope :by_id_and_token, ->id, token { where(project_id: id, token:) }
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -4,34 +4,42 @@ require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
   describe '.by_id_and_token' do
-    test 'finds project by id and token' do
+    test 'returns project when valid id and token are provided' do
       expected_project = create(:project)
       project = Project.by_id_and_token(expected_project.project_id, expected_project.token).take
 
       assert_equal expected_project, project
     end
 
-    test 'returns nil for invalid project id' do
+    test 'returns nil when project id does not exist' do
       project = create(:project)
 
       assert_nil Project.by_id_and_token(ULID.generate, project.token).take
     end
 
-    test 'returns nil for invalid token' do
+    test 'returns nil when token is invalid' do
       project = create(:project)
 
-      assert_nil Project.by_id_and_token(project.project_id, 'invalid').take
+      assert_nil Project.by_id_and_token(project.project_id, SecureRandom.hex).take
     end
   end
 
-  test 'should not save project without project_id' do
+  test 'validates presence of project_id' do
     project = Project.new
 
     assert_not project.save
     assert_includes project.errors.messages[:project_id], "can't be blank"
   end
 
-  test 'should not save project without token' do
+  test 'validates uniqueness of project_id' do
+    existing_project = create(:project)
+    project = Project.new(project_id: existing_project.project_id)
+
+    assert_not project.save
+    assert_includes project.errors.messages[:project_id], 'has already been taken'
+  end
+
+  test 'validates presence of token' do
     project = Project.new
 
     assert_not project.save
