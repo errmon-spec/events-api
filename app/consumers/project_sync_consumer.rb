a86b6c1ecde2e0f20644a864c0be7f3bb6a24bd2
Lucas Damasceno <lucas.damasceno@al.infnet.edu.br>
2024-03-23 14:07:00 -0300

    fix: add routing key to ProjectSyncConsumer

--- app/consumers/project_sync_consumer.rb
+++ app/consumers/project_sync_consumer.rb
@@ -4,6 +4,7 @@ class ProjectSyncConsumer
   include Sneakers::Worker
 
   from_queue :project_sync,
+    routing_key: 'project.updated',
     content_type: 'application/json',
     timeout_job_after: 10.seconds.in_milliseconds,
     retry_timeout: 5.seconds.in_milliseconds,
